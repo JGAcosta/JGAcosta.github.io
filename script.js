b235e1d13b6b88332ba8e4dc0f9694bb1c17fcf1
Jacob G Acosta <46684007+JGAcosta@users.noreply.github.com>
2019-02-25 19:12:54 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,43 @@
 $(document).ready(function(){
+  //current time
+  //update every 10 seconds;
+  setCurrentTime();
+  setInterval(function(){
+    setCurrentTime();
+  },10*1000);
+
+  var username = getCookie('username');
+  //check cookie
+  if(username){
+    $('.greeting').css('display','inline-block');
+    $('.user-name').css('display','none');
+    $('.greeting').html(`Hello <span class="stored-name">${username}</span>.`);
+  }else{
+    $('.greeting').css('display','none');
+    $('.user-name').css('display','inline-block');
+    $('.greeting').html(`What's your name?`);
+  }
+
   $('.user-name').keypress(function(e) {
     if(e.which == 13) {
-      var value = e.target.value;
-      if(!value) return;
+      var username = e.target.value;
+      if(!username) return;
       $('.user-name').fadeOut(function(){
-        $('.greeting').html(`Hello ${value}.`);
+        $('.greeting').html(`Hello ${username}.`);
         $('.greeting').fadeIn(function(){
-          setCookie('username', value,365);
+          setCookie('username', username,365);
         });
       });
     }
   });
 
 });
+
+function setCurrentTime(){
+  var now = new Date();
+  $('.time').html(now.getHours()+":"+now.getMinutes())
+  $('.date').html(now.toLocaleDateString('en-US', { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' }));
+}
 function setCookie(cname,cvalue,exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
